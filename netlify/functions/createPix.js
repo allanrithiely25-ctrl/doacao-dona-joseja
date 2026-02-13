0461fd6e06f00ee88d04e1872b9dc9d371096798
allanrithiely25-ctrl <allanrithiely25@gmail.com>
2026-02-13 15:54:08 -0300

    Update createPix.js

--- netlify/functions/createPix.js
+++ netlify/functions/createPix.js
@@ -1,23 +1,33 @@
 exports.handler = async (event) => {
-  const { amount } = JSON.parse(event.body);
+  try {
+    const { amount } = JSON.parse(event.body);
 
-  const response = await fetch("https://api.elitepaybr.com/api/v1/deposit", {
-    method: "POST",
-    headers: {
-      "x-client-id": process.env.CLIENT_ID,
-      "x-client-secret": process.env.CLIENT_SECRET,
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify({
-      amount,
-      paymentMethod: "PIX"
-    })
-  });
+    const response = await fetch("https://api.elitepaybr.com/api/v1/deposit", {
+      method: "POST",
+      headers: {
+        "x-client-id": process.env.CLIENT_ID,
+        "x-client-secret": process.env.CLIENT_SECRET,
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        amount: amount,
+        description: "Doação - Cirurgia Dona Joseja",
+        payerName: "Doacao Site",
+        payerDocument: "00000000000"
+      })
+    });
 
-  const data = await response.json();
+    const data = await response.json();
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify(data)
-  };
+    return {
+      statusCode: 200,
+      body: JSON.stringify(data)
+    };
+
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: "Erro ao criar pagamento" })
+    };
+  }
 };
